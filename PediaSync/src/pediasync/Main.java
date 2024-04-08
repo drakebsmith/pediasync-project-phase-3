828d8e370f7570630cbafa4014820911fd6553b0
Drake Smith <Drake Smith@DESKTOP-KKA8PJG>
2024-04-07 20:11:33 -0700

    Basic Main implementation added.

--- PediaSync/src/pediasync/Main.java
+++ PediaSync/src/pediasync/Main.java
@@ -1,5 +1,24 @@
 package pediasync;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Main extends Application
+{
+	public void start(Stage primary_stage) 
+	{
+		Pane pane = new Pane();
+		
+		Scene scene = new Scene(pane, 620, 400);
+		
+		primary_stage.setScene(scene);
+		
+		primary_stage.show();
+	}
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
 }
